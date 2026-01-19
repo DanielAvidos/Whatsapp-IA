34f8df6447814fd3b7789bf3f555d2a0e0c15ae6
danrro tecnology <danrro.tecnology@gmail.com>
2026-01-19 18:13:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6317141337-13e75",
+  "appId": "1:701554958520:web:606c84c37b5d1a92d3d44d",
+  "apiKey": "AIzaSyAFFXx11CNmtlRPYVMugKMzzD8fWJ8AmqQ",
+  "authDomain": "studio-6317141337-13e75.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "701554958520"
+};
